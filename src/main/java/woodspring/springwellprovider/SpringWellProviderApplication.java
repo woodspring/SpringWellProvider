8dd48e8cb30f220956c10681c01bed3fa62d86bb
woodspring <woodspring.toronto@gmail.com>
2021-11-13 20:20:08 -0500

    start Provider project

---
+++ src/main/java/woodspring/springwellprovider/SpringWellProviderApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springwellprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWellProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWellProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodspring/springwellprovider/SpringWellProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springwellprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWellProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
